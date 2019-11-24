d6d3111cbc1646e2e0cffc8a39ce314cd4e1f32b
Mitchell DeJonghe <mitchell.l.dejonghe@wmich.edu>
2019-11-23 19:04:31 -0500

    Order Page updates

---
+++ src/php/nav.inc.php
@@ -0,0 +1,35 @@
+<?php
+echo'
+<div class="container-fluid bg-primary">
+<div class="container">
+    <nav class="navbar navbar-expand-md navbar-dark">
+        <a class="navbar-brand" href="">BUMBU</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNav"
+            aria-controls="collapsibleNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end text-uppercase" id="collapsibleNav">
+            <ul class="navbar-nav justify-content-end text-right ">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="">Menu</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link " href="">Order</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="">Locations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</div>'
+
+?>
--- src/php/order.php
+++ src/php/order.php
@@ -15,36 +15,8 @@
 </head>
 
 <body>
-    <div class="container-fluid bg-primary">
-        <div class="container">
-            <nav class="navbar navbar-expand-md navbar-dark">
-                <a class="navbar-brand" href="">BUMBU</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNav" aria-controls="collapsibleNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse justify-content-end text-uppercase" id="collapsibleNav">
-                    <ul class="navbar-nav justify-content-end text-right ">
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="">Menu</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link " href="">Order</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="">Locations</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="">About</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </div>
+    
+<?php include 'nav.inc.php'?>
 
     <div class="container">
         <br>
