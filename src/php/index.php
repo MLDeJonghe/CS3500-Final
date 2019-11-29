c630b6dffe373cc822be78ea18d566e8a15e40b2
CawFee <darrylmingsen.lee@wmich.edu>
2019-11-29 18:04:02 -0500

    Stupid Retard Shit

--- src/php/index.php
+++ src/php/index.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/bootstrap4.css">
-
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -21,6 +22,10 @@
     include "nav.inc.php";
     ?>
     <div class="container" >
+        <h1 class="text-uppercase">
+            Welcome to Bumbu
+        </h1>
+        <h4>Bumbu Dropping Deals</h4>
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -29,14 +34,14 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="../images/Me.jpg" alt="First slide" height="25">
+                   <a href="menu.php"><img class="d-block w-100" src="../images/Me.jpg" alt="First slide" height="250" width="400"></a>
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="../images/pic.jpg" alt="Second slide">
+                    <img class="d-block w-100" src="../images/pic.jpg" alt="Second slide" height="250">
                 </div>
-                <!-- <div class="carousel-item">
-                <img class="d-block w-100" src="Me.jpg" alt="Third slide">
-                </div>  -->
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="../images/Me.jpg" alt="Third slide" height="250">
+                </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -47,8 +52,122 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-
-        <div>
+        
+        <p data-aos="flip-down">
+            Here at Bumbu, we aim to bring our guest the most authentic Indonesian flavors as possible.
+            You will be indulged with mouth watering traditional Indonesian dishes and the aroma of spices.
+            At Bumbu, we ensure only the highest quality ingredients will be served to our guests.
+            Our restaurants feature an impeccable atmosphere that will bolster your experience and leave you wanting more.
+            Visit us at one of our three Michigan locations today!
+        </p>
+        <div class="pt-4">
+            <h2>Review</h2>
+            <hr>
+            <div id="review" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#review" data-slide-to="0" class="active"></li>
+                <li data-target="#review" data-slide-to="1"></li>
+                <li data-target="#review" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="card mb-1 " data-aos="fade-up">
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 ">
+                            <p>Good food.</p>
+                                <footer class="blockquote-footer">Robbie Weirdick <cite title="Source Title">Source Title</cite></footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div class="card  mb-1"  data-aos="fade-up">
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                            <p>No lamb sauce.</p>
+                                <footer class="blockquote-footer">Gordon Ramsey <cite title="Source Title">Source Title</cite></footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="card mb-1" data-aos="fade-up">
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                            <p>Chef is good</p>
+                                <footer class="blockquote-footer">Sherlock Holmes <cite title="Source Title">Source Title</cite></footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div class="card  mb-1"  data-aos="fade-up">
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                            <p>That is my spot</p>
+                                <footer class="blockquote-footer">Sheldon Copper <cite title="Source Title">Source Title</cite></footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#review" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon border-primary" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#review" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!-- <div>
+                <div class="card mb-1" data-aos="fade-up">
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="card  mb-1"  data-aos="fade-up">
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="card  mb-1"  data-aos="fade-up">
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="card  mb-1"  data-aos="fade-up">
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="card mb-1"  data-aos="fade-up">
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="card mb-1"  data-aos="fade-up">
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div> -->
         </div>
     </div>
+    <script>
+        AOS.init();
+    </script>
 </body>
--- src/php/location.php
+++ src/php/location.php
@@ -9,13 +9,15 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/bootstrap4.css">
     <link rel="stylesheet" href="../css/location.css">
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>Bumbu Indonesian Cuisine</title>
 </head>
 
 <body>
-    <?php 
-        include "nav.inc.php";
+    <?php
+    include "nav.inc.php";
     ?>
     <!-- <div class="container-fluid bg-primary">
         <div class="container">
@@ -28,19 +30,19 @@
                 <div class="collapse navbar-collapse justify-content-end text-uppercase" id="collapsibleNav">
                     <ul class="navbar-nav justify-content-end text-right ">
                         <li class="nav-item">
-                            <a class="nav-link" href="">Home</a>
+                            <a class="navlink" href="">Here</a>
                         </li>
                         <li class="nav-item active">
-                            <a class="nav-link " href="">Menu</a>
+                            <a class="navlink " href="">Menu</a>
                         </li>
                         <li class="nav-item ">
-                            <a class="nav-link " href="">Order</a>
+                            <a class="navlink " href="">Order</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link " href="">Locations</a>
+                            <a class="navlink " href="">Locations</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link " href="">About</a>
+                            <a class="navlink " href="">About</a>
                         </li>
                     </ul>
                 </div>
@@ -49,6 +51,65 @@
     </div> -->
 
     <div class="container">
-        
+        <div class="row">
+            <div class="col-sm-10 mx-auto">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div class="mapouter mx-auto">
+                            <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=25%2CHalaman%20Bukit%20Gambier&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.org">embed map on contact page google maps</a></div>
+                            <style>
+                                .mapouter {
+                                    position: relative;
+                                    text-align: right;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+
+                                .gmap_canvas {
+                                    overflow: hidden;
+                                    background: none !important;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+                            </style>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <div class="mapouter mx-auto">
+                            <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Kalamazoo%20Uni&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.org">embed map on contact page google maps</a></div>
+                            <style>
+                                .mapouter {
+                                    position: relative;
+                                    text-align: right;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+
+                                .gmap_canvas {
+                                    overflow: hidden;
+                                    background: none !important;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+                            </style>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
