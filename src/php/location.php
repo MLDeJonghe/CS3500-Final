42e4ca5aff5f3befd67b4e6196d414018873dc88
AsianCawFee <darryllee1999@gmail.com>
2019-11-30 17:03:41 -0500

    Tush

--- src/php/location.php
+++ src/php/location.php
@@ -21,22 +21,118 @@
     ?>
 
     <div class="container">
-        <h1>Our Locations</h1>
+        <div class="row">
+            <div class="col">
+                <p class="display-3">Locations </p>
+                    <hr class="bg-primary my-4">
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-4">
+                <div class="list-group" id="list-tab" role="tablist">
+                    <a class="list-group-item list-group-item-action active" id="list-KZ-list" data-toggle="list" href="#list-KZ" role="tab" aria-controls="KZ">
+                        Kalamazoo
+                    </a>
+                    <a class="list-group-item list-group-item-action" id="list-GR-list" data-toggle="list" href="#list-GR" role="tab" aria-controls="GR">
+                        Grand Rapids
+                    </a>
+                    <a class="list-group-item list-group-item-action" id="list-L-list" data-toggle="list" href="#list-L" role="tab" aria-controls="L">
+                        Lansing
+                    </a>
+
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="list-KZ" role="tabpanel" aria-labelledby="list-KZ-list">
+                        <div class="mapouter">
+                            <div class="gmap_canvas">
+                                <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=5498%20Minuteman%20Cir%20Kalamazoo%2C%20Michigan(MI)%2C%2049009&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                                <a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/">news</a>
+                            </div>
+                            <style>
+                                .mapouter {
+                                    position: relative;
+                                    text-align: right;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+
+                                .gmap_canvas {
+                                    overflow: hidden;
+                                    background: none !important;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+                            </style>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="list-GR" role="tabpanel" aria-labelledby="list-GR-list">
+                        <div class="mapouter">
+                            <div class="gmap_canvas">
+                                <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=720%204th%20St%20NW%20Grand%20Rapids%2C%20Michigan(MI)%2C%2049504&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                                <a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/">news</a>
+                            </div>
+                            <style>
+                                .mapouter {
+                                    position: relative;
+                                    text-align: right;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+
+                                .gmap_canvas {
+                                    overflow: hidden;
+                                    background: none !important;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+                            </style>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="list-L" role="tabpanel" aria-labelledby="list-L-list">
+                        <div class="mapouter">
+                            <div class="gmap_canvas">
+                                <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=8630%20Camellia%20St%20Lansing%2C%20Michigan(MI)%2C%2048917&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                                <a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/">news</a>
+                            </div>
+                            <style>
+                                .mapouter {
+                                    position: relative;
+                                    text-align: right;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+
+                                .gmap_canvas {
+                                    overflow: hidden;
+                                    background: none !important;
+                                    height: 500px;
+                                    width: 600px;
+                                }
+                            </style>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <h1>Our Locations</h1>
         <div class="row border">
             <div class="col-sm-10">
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                        <a class="nav-link active" id=-KZ-tab" data-toggle="tab" href="-KZ" role="tab" aria-controls=-KZ" aria-selected="true"-KZ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                        <a class="nav-link" id="GR-tab" data-toggle="tab" href="#GR" role="tab" aria-controls="GR" aria-selected="false">GR</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
                     </li>
                 </ul>
                 <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="tab-pane fade show active" id=-KZ" role="tabpanel" aria-labelledby=-KZ-tab">
                         <div class="mapouter mx-auto">
                             <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=25%2CHalaman%20Bukit%20Gambier&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.org">embed map on contact page google maps</a></div>
                             <style>
@@ -59,7 +155,7 @@
                             Contact Number: 22222222
                         </aside>
                     </div>
-                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                    <div class="tab-pane fade" id="GR" role="tabpanel" aria-labelledby="GR-tab">
                         <div class="mapouter mx-auto">
                             <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Kalamazoo%20Uni&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.org">embed map on contact page google maps</a></div>
                             <style>
@@ -84,6 +180,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </body>
